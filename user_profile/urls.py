5735c0ec13385f1a90528b581a57d9dc4a2e8d53
Abin Wilson <abinwilson122@gmail.com>
2024-06-10 15:35:25 +0530

    add profile updates

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-
+from . import views
 app_name = "profile"
 
 urlpatterns = [
-    
+    path('', views.view_profile, name="view_profile"),
+    path('update/',views.update_profile, name='update_profile'),
+    path('password/',views.update_password, name='update_password'),
+
 ]
--- user_profile/views.py
+++ user_profile/views.py
@@ -1,3 +1,67 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
-# Create your views here.
+@login_required
+def view_profile(request):
+    return render(request, 'view_profile.html', {
+        'title' : 'Profile',
+    })
+
+@login_required
+def update_profile(request):
+    if request.method == 'POST':
+        try:
+            first_name = request.POST['first_name']
+            last_name = request.POST['last_name']
+            username = request.POST['username']
+            email = request.POST['email']
+            gender = request.POST['gender']
+            image = request.FILES.get('image')
+            
+            # Profile Model
+            if image:
+                request.user.profile.image = image
+            request.user.profile.gender = gender
+            request.user.profile.save()
+
+            # User Model
+            request.user.first_name = first_name
+            request.user.last_name = last_name
+            request.user.username = username
+            request.user.email = email
+            request.user.save()
+
+            messages.success(request, "Profile updated successfully! Your changes have been saved.")
+            return redirect('profile:update_profile')
+        except Exception as error:
+            messages.error(request, f"Failed to update profile. {error}")
+    return render(request, 'update_profile.html', {
+        'title' : 'Update Profile'
+    })
+
+@login_required
+def update_password(request):
+    if request.method == 'POST':
+        try:
+            old_password = request.POST['old_password']
+            new_password = request.POST['new_password']
+            confirm_new_password = request.POST['confirm_new_password']
+            
+            if request.user.check_password(old_password):
+                if new_password == confirm_new_password:
+                    request.user.set_password(new_password)
+                    request.user.save()
+                    messages.success(request, 'Password updated successfully!')
+                    return redirect('core:signin')
+                else:
+                    messages.error(request, 'Failed to update password. Old password does not match.')
+                    return redirect('profile:update_password')
+            else:
+                messages.error(request, 'Failed to update password. Old password does not match.')
+                return redirect('profile:update_password')
+        except Exception as error:
+            messages.error(request,f'Failed to update password {error}')
+    return render(request, 'update_password.html', {
+        'title':'change password' 
+    })
